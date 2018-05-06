ff92b8b60aa78043cf45cc3372477ab3813e4d51
Roman <asoil2256@gmail.com>
2018-05-06 14:57:10 +0400

    SocialNetwork

---
+++ SocialNetwork/SocialNetwork/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetwork.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string SecondName { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
