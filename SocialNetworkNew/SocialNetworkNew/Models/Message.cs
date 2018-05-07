4ba3a2902a8014259d00feb688c21f82bb318783
Roman <asoil2256@gmail.com>
2018-05-07 16:46:15 +0400

    Новое решение

---
+++ SocialNetworkNew/SocialNetworkNew/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetworkNew.Models
+{
+    public class Message
+    {
+        public Guid Id { get; set; }
+        public string Text { get; set; }
+        public string Author { get; set; }
+    }
+}
---
+++ SocialNetworkNew/SocialNetworkNew/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SocialNetworkNew.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string Phone { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
